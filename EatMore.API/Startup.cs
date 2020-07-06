93ac803ece77a6a434646a02eafc400c0c682b88
Kanchan Kiran <kanchan.kiran@hashedin.com>
2020-07-07 00:04:09 +0530

    first commit

---
+++ EatMore.API/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EatMore.API
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<AppContext>(opt => opt.UseInMemoryDatabase("EatMoreDb"));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseGraphiQl();
+
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+
+                var db = serviceScope.ServiceProvider.GetService<AppContext>();
+                DataSeed.Run(db);
+            }
+
+            app.UseMvc();
+        }
+    }
+}
